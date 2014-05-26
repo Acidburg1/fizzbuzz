fe90a87d19cc713e5f0fc391f8761ca9e6215626
acidburg <acidburg@localhost>
2014-05-26 19:23:48 +0200

    Setup del entorno de test

---
+++ src/FizzBuzz/FizzBuzz.php
@@ -0,0 +1,11 @@
+<?php
+namespace FizzBuzz;
+
+class FizzBuzz {
+
+    public function devuelve($param0) {
+        
+    }
+
+    //put your code here
+}
---
+++ src/FizzBuzz/Test/FizzBuzzTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FizzBuzz\Test;
+
+use FizzBuzz\FizzBuzz;
+
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetup()
+    {
+        $this->assertTrue(true);
+    }
+}
