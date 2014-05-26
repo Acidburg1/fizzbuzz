1076b3158e80d3d91372a145cde580aa263dcc50
acidburg <acidburg@localhost>
2014-05-26 19:31:59 +0200

    Ya sabe cuál es la respuesta del primer niño

--- src/FizzBuzz/FizzBuzz.php
+++ src/FizzBuzz/FizzBuzz.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace FizzBuzz;
 
 class FizzBuzz {
 
     public function devuelve($param0) {
-        
+        return 1;
     }
 
     //put your code here
--- src/FizzBuzz/Test/FizzBuzzTest.php
+++ src/FizzBuzz/Test/FizzBuzzTest.php
@@ -4,10 +4,15 @@ namespace FizzBuzz\Test;
 
 use FizzBuzz\FizzBuzz;
 
-class FizzBuzzTest extends \PHPUnit_Framework_TestCase
-{
-    public function testSetup()
-    {
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase {
+
+    public function testSetup() {
         $this->assertTrue(true);
     }
+
+    public function testElPrimerElementoEs1() {
+        $fizzBuzz = new FizzBuzz();
+        $this->assertEquals(1, $fizzBuzz->devuelve(1));
+    }
+
 }
